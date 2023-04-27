19346f364ecc2dc7082ecf1304acae7bcbbf7e5e
Chantal <chantalgomez@pursuit.org>
2023-04-27 12:27:16 -0400

    key

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <div>
         Current API KEY:{" "}
         {process.env.NODE_ENV === "development"
-          ? process.env.REACT_APP_DEVELOPMENT_API_KEY
+          ? process.env.REACT_APP_DEPLOYMENT_API_KEY
           : process.env.REACT_APP_PRODUCTION}
       </div>
     </div>
